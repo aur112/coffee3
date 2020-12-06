d5846db9e2904e27c20f7a9fc6f328909404778b
aur112 <75263972+aur112@users.noreply.github.com>
2020-12-06 21:35:13 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import sys
+from random import randrange
+from PyQt5 import QtGui, uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+from PyQt5.QtGui import QPainter, QPen, QBrush, QColor
+from PyQt5.QtCore import Qt
+from UI import Ui_MainWindow
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.b = False
+        self.pushButton.clicked.connect(self.circles)
+
+    def circles(self):
+        self.b = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.b:
+            painter = QPainter(self)
+            painter.setPen(QPen(Qt.black, 1, Qt.SolidLine))
+            for i in range(20):
+                painter.setBrush(
+                    QBrush(QColor(randrange(0, 255), randrange(0, 255), randrange(0, 255)), Qt.SolidPattern))
+                r = randrange(5, 50)
+                painter.drawEllipse(randrange(0, self.width()), randrange(0, self.height()), r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
