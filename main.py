292085a874ce1f4c3d4dac0881e3c68032c08bfe
aur112 <abilmansur2005@gmail.com>
2020-12-10 21:52:00 +0600

    first

--- main.py
+++ main.py
@@ -2,30 +2,31 @@ import sys
 import sqlite3
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QWidget
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.sqnames = {
+            0: "name",
+            1: "roast",
+            2: "type",
+            3: "description",
+            4: "cost",
+            5: "size",
+        }
         uic.loadUi('main.ui', self)
         self.loadTable('coffee.sqlite')
+        self.tableWidget.cellChanged.connect(self.edit)
+        self.add.clicked.connect(self.new)
 
     def loadTable(self, table_name):
-        con = sqlite3.connect(table_name)
-        cur = con.cursor()
-        info = list(cur.execute("""SELECT * FROM coffee""").fetchall())
-        print(info)
-        for i in range(len(info)):
-            info[i] = list(info[i])
-            info[i][1] = cur.execute("""SELECT name FROM sorts
-                                    WHERE id=?""", (info[i][1],)).fetchone()[0]
-            info[i][2] = cur.execute("""SELECT roast FROM roast_degree
-                                                WHERE id=?""", (info[i][2],)).fetchone()[0]
-            info[i][3] = cur.execute("""SELECT type FROM type
-                                                            WHERE id=?""", (info[i][3],)).fetchone()[0]
-        self.tableWidget.setColumnCount(7)
-        self.tableWidget.setHorizontalHeaderLabels(['id', 'название сорта', 'степень обжарки', 'тип', 'описание вкуса',
+        self.con = sqlite3.connect(table_name)
+        self.cur = self.con.cursor()
+        info = list(self.cur.execute("""SELECT name, roast, type, description, cost, size FROM coffee""").fetchall())
+        self.tableWidget.setColumnCount(6)
+        self.tableWidget.setHorizontalHeaderLabels(['название сорта', 'степень обжарки', 'тип', 'описание вкуса',
                                                     'цена', 'объем упаковки'])
         self.tableWidget.setRowCount(0)
         for i, row in enumerate(info):
@@ -33,6 +34,41 @@ class MyWidget(QMainWindow):
             for j, elem in enumerate(row):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
         self.tableWidget.resizeColumnsToContents()
+
+    def edit(self, row, column):
+        if column > 3:
+            s = """UPDATE coffee SET """ + self.sqnames[column]
+            self.cur.execute(s + """= ?
+                              WHERE id = ?""", (int(self.tableWidget.item(row, column).text()), row + 1))
+            self.con.commit()
+        else:
+            s = """UPDATE coffee SET """ + self.sqnames[column]
+            self.cur.execute(s + """= ?
+                  WHERE id = ?""", (self.tableWidget.item(row, column).text(), row + 1))
+            self.con.commit()
+
+    def new(self):
+        self.add = Add()
+        self.add.show()
+
+
+class Add(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.pushButton.clicked.connect(self.add_new)
+
+    def add_new(self):
+        self.con = sqlite3.connect('coffee.sqlite')
+        self.cur = self.con.cursor()
+        s = len(self.cur.execute("""SELECT * FROM coffee""").fetchall())
+        self.cur.execute("""INSERT INTO coffee(id, name, roast, type, description, cost, size) 
+                                            VALUES(?, ?, ?, ?, ?, ?, ?)""", (s + 1, self.lineEdit_2.text(),
+                                        self.lineEdit_3.text(), self.lineEdit_4.text(), self.lineEdit_5.text(),
+                                                            self.lineEdit_6.text(), self.lineEdit_7.text()))
+        self.con.commit()
+        ex.loadTable('coffee.sqlite')
+        self.close()
 
 
 if __name__ == '__main__':
